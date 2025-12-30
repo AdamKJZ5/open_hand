4ffbe8443fa34437a1269612c40d5b2865709d54
AdamKJZ5 <adamkaljemal@gmail.com>
2025-12-30 04:11:09 -0800

    fix: resolve merge conflicts in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,16 @@
 import express, {Application, Request, Response, NextFunction } from 'express';
-<<<<<<< Updated upstream
-import dotenv from 'gotenv';
-import cars from 'cors';
-import mongoose from 'mogoose';
-import authRoutes from './routes/authRoutes';
-=======
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
->>>>>>> Stashed changes
 
 dotenv.config();
 
 const app: Application = express();
 
-<<<<<<< Updated upstream
-app.use('/api/auth', authRoutes);
-app.use(cors());
-=======
 app.use(cors({
   origin: 'http://localhost:5173'
 }));
->>>>>>> Stashed changes
+
 app.use(express.json());
 
 const connectDB = async() => {
@@ -36,8 +25,17 @@ const connectDB = async() => {
 
 connectDB();
 
+app.use('/api/auth', authRoutes);
+
 app.get('/health', (req: Request, res: Response) => {
-  res.status(200).json({ status: UP, message: 'Server is healthy' })
+  res.status(200).json({ status: UP, message: 'Server is healthy' });
+  });
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 });
 
